b098bec5382f6edc61524514fa95815864cebc9c
pythias <pythias@gmail.com>
2019-05-30 11:15:06 +0800

    修复相对路径问题

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,7 @@ RUN_DIR=`dirname "$0"`
 
 JAVA_OPTS=" -Xms256M -Xmx512M "
 AGENT_OPTS=" -Dplugin.dir=${RUN_DIR}/plugins"
-OPTS="${JAVA_OPTS} ${AGENT_OPTS} -javaagent:agent-bootstrap-0.0.1.jar -jar agent-application-0.0.1.jar"
+OPTS="${JAVA_OPTS} ${AGENT_OPTS} -javaagent:${RUN_DIR}/agent-bootstrap-0.0.1.jar -jar ${RUN_DIR}/agent-application-0.0.1.jar"
 
 if [[ -d "${JAVA_HOME}" ]]; then
     ${JAVA_HOME}/bin/java $OPTS
